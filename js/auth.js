817ce2d097b0446c674bf6a72a2e17cd99da4d32
littleban0305 <littleban0305@gmail.com>
2026-07-21 12:52:41 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -21,3 +21,77 @@ if(loginBtn){
     );
 
 }
+
+function handleCredentialResponse(
+    response
+){
+
+    const payload =
+    JSON.parse(
+        atob(
+            response.credential
+            .split(".")[1]
+        )
+    );
+
+    console.log(
+        payload
+    );
+
+    localStorage.setItem(
+        "memberEmail",
+        payload.email
+    );
+
+    localStorage.setItem(
+        "memberGoogleName",
+        payload.name
+    );
+
+    checkMember(
+        payload.email
+    );
+
+}
+
+function checkMember(
+    email
+){
+
+    fetch(
+        "你的AppsScript網址"
+        +
+        "?action=checkMember"
+        +
+        "&email="
+        +
+        encodeURIComponent(
+            email
+        )
+    )
+    .then(
+        response=>response.text()
+    )
+    .then(
+        result=>{
+
+            if(
+                result === "exists"
+            ){
+
+                window.location.href =
+                "profile.html";
+
+            }
+
+            else{
+
+                window.location.href =
+                "complete-profile.html";
+
+            }
+
+        }
+    );
+
+}
