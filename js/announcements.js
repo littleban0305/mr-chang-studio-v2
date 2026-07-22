547eb6ddaa761d8acc3c374ecd56a9d52b0dc352
littleban0305 <littleban0305@gmail.com>
2026-07-22 09:46:42 +0800

    Add announcements fetching and rendering functionality

---
+++ js/announcements.js
@@ -0,0 +1,89 @@
+fetch(
+    "https://script.google.com/macros/s/AKfycbyjyjZ891V-eMkAtImiB1Cl3fUTubcDhb_6sF6MPezzAdaIXr3_N1q5kZ5SbHpPHDhC/exec?action=getAnnouncements"
+)
+.then(
+    response=>response.json()
+)
+.then(
+    announcements=>{
+
+        const list =
+        document.getElementById(
+            "allAnnouncementList"
+        );
+
+        if(
+            announcements.length
+            ===
+            0
+        ){
+
+            list.innerHTML =
+            "目前沒有公告";
+
+            return;
+
+        }
+
+        list.innerHTML =
+        announcements
+        .map(
+            a=>{
+
+                const typeClass = {
+
+                    "系統公告":
+                    "system-tag",
+
+                    "活動公告":
+                    "activity-tag",
+
+                    "維護公告":
+                    "maintenance-tag",
+
+                    "工作室公告":
+                    "studio-tag"
+
+                }[a.type]
+                ||
+                "system-tag";
+
+                return `
+
+                <div
+                    class="announcement-card"
+                >
+
+                    <span
+                        class="announcement-date"
+                    >
+                        ${a.date}
+                    </span>
+
+                    <div
+                        class="
+                        announcement-type
+                        ${typeClass}
+                        "
+                    >
+                        ${a.type}
+                    </div>
+
+                    <h3>
+                        ${a.title}
+                    </h3>
+
+                    <p>
+                        ${a.content}
+                    </p>
+
+                </div>
+
+                `;
+
+            }
+        )
+        .join("");
+
+    }
+);
