7b3b717dadcd9853f972747b9f75157ec00fc707
littleban0305 <littleban0305@gmail.com>
2026-07-22 11:12:35 +0800

    Create edit-profile.js

---
+++ js/edit-profile.js
@@ -0,0 +1,141 @@
+document.getElementById(
+    "memberName"
+).value =
+localStorage.getItem(
+    "memberName"
+) || "";
+
+document.getElementById(
+    "memberPhone"
+).value =
+localStorage.getItem(
+    "memberPhone"
+) || "";
+
+document.getElementById(
+    "memberBirthday"
+).value =
+localStorage.getItem(
+    "memberBirthday"
+) || "";
+
+const saveBtn =
+document.getElementById(
+    "saveProfileBtn"
+);
+
+saveBtn.addEventListener(
+    "click",
+    ()=>{
+
+        const name =
+        document.getElementById(
+            "memberName"
+        ).value;
+
+        const phone =
+        document.getElementById(
+            "memberPhone"
+        ).value;
+
+        const birthday =
+        document.getElementById(
+            "memberBirthday"
+        ).value;
+
+        const email =
+        localStorage.getItem(
+            "memberEmail"
+        );
+
+        if(
+            !name ||
+            !phone ||
+            !birthday
+        ){
+
+            alert(
+                "請填寫完整資料"
+            );
+
+            return;
+
+        }
+
+        const formData =
+        new URLSearchParams();
+
+        formData.append(
+            "action",
+            "registerMember"
+        );
+
+        formData.append(
+            "email",
+            email
+        );
+
+        formData.append(
+            "name",
+            name
+        );
+
+        formData.append(
+            "phone",
+            phone
+        );
+
+        formData.append(
+            "birthday",
+            birthday
+        );
+
+        showLoading(
+            "正在建立會員..."
+        );
+        
+        fetch(
+            "https://script.google.com/macros/s/AKfycbyjyjZ891V-eMkAtImiB1Cl3fUTubcDhb_6sF6MPezzAdaIXr3_N1q5kZ5SbHpPHDhC/exec",
+            {
+                method:"POST",
+                body:formData
+            }
+        )
+        .then(
+            response=>response.text()
+        )
+        .then(
+            ()=>{
+
+                localStorage.setItem(
+                    "memberName",
+                    name
+                );
+
+                localStorage.setItem(
+                    "memberPhone",
+                    phone
+                );
+
+                localStorage.setItem(
+                    "memberBirthday",
+                    birthday
+                );
+
+                localStorage.setItem(
+                    "isLogin",
+                    "true"
+                );
+
+                alert(
+                    "會員資料已更新"
+                );
+
+                location.href =
+                "profile.html";
+
+            }
+        );
+
+    }
+);
